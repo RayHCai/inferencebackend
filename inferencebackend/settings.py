8d8738b3707d47ad5dff8f6d782f1517a18bb05f
Ray Cai Flanagan <hjkareus@gmail.com>
2022-11-11 08:47:59 -0500

    pg

--- inferencebackend/settings.py
+++ inferencebackend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'inferencebackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'AdZFHafT7Kn8bKgI3QRq',
+        'HOST': 'containers-us-west-87.railway.app',
+        'PORT': '7442',
     }
 }
 
